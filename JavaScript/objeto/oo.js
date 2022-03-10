3a92e4510010f56b5cbe132478733dbdb430d7c0
Vinicius Santos <santosviniv@gmail.com>
2022-03-10 13:14:58 -0300

    Primeiros arquivos do capitulo Fundamentos: Objeto

---
+++ JavaScript/objeto/oo.js
@@ -0,0 +1,23 @@
+// Código não executável
+
+// PARADIGMA PROCEDURAL
+processamento(valor1, valor2, valor3)
+
+// OO
+objeto = {
+  //Objeto funciona como uma cápsula que agrupa comportamentos e atributos
+  valor1,
+  valor2,
+  valor3,
+  processamento() {
+    //...
+  }
+}
+
+objeto.processamento() // Foco passou a ser o objeto
+
+// Princípios importantes:
+// 1. Abstração
+// 2. Encapsulamento
+// 3. Herança (prototype) --- Priorize a composição do que a herança!
+// 4. Polimorfismo
